c8e32f6178d896364c2cfcdf5a45bf61f130bedc
KamiFightingSpirit <55668557+KamiFightingSpirit@users.noreply.github.com>
2021-07-08 10:44:10 -0700

    Update getLCD.js

--- src/helperfunctions/getLCD.js
+++ src/helperfunctions/getLCD.js
@@ -1,6 +1,7 @@
-const { getGCD } = require("./getGCD");
 //returns the Lowest Common Divisor of two numbers
 //Derived from: https://www.geeksforgeeks.org/least-common-denominator-lcd/
+const { getGCD } = require("./getGCD");
+
 const getLCD = (num1, num2) => {
   return (num1 * num2) / getGCD(num1, num2);
 };
