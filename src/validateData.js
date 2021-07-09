da3c0ba2e606bb79a07484630d365212566b5164
KamiFightingSpirit <55668557+KamiFightingSpirit@users.noreply.github.com>
2021-07-09 07:32:40 -0700

    Throwing a better error classname

--- src/validateData.js
+++ src/validateData.js
@@ -1,6 +1,7 @@
 const { getOrdinalSuffix } = require("./helperfunctions/getOrdinalSuffix");
 const { RegexPackage } = require("./classes/RegexTests.js");
 const { AssertionError } = require("assert");
+const { SyntaxError } = require("./classes/SyntaxError");
 
 //If no arguments, exit immediately
 const validateData = (input) => {
@@ -32,46 +33,46 @@ const validateData = (input) => {
     //Make sure nothing is divided by zero
     if (parseInt(input[i]) === 0) {
       if (input[i - 1] === "/") {
-        throw new AssertionError({
-          message: `Error: Cannot divide by zero. The error was found in the ${indexOfError} argument: ${currentArg}`,
-        });
+        throw new SyntaxError(
+          `Error: Cannot divide by zero. The error was found in the ${indexOfError} argument: ${currentArg}`
+        );
       }
       //Make sure there are no leading zeros
     } else if (parseInt(input[i][0]) === 0) {
-      throw new AssertionError({
-        message: `Error: Inputs should not start with zero. The error was found in the ${indexOfError} argument: ${currentArg}`,
-      });
+      throw new SyntaxError(
+        `Error: Inputs should not start with zero. The error was found in the ${indexOfError} argument: ${currentArg}`
+      );
     }
     //Check the first argument to make it isn't a math operator
     if (i === 0 && operatorRegexTest) {
-      throw new AssertionError({
-        message: `Error: Your input starts with a ${currentArg}, your first argument should be a number`,
-      });
+      throw new SyntaxError(
+        `Error: Your input starts with a ${currentArg}, your first argument should be a number`
+      );
     }
     //Check to make sure the last argument isn't a math operator
     if (i === input.length - 1 && operatorRegexTest) {
-      throw new AssertionError({
-        message: `Error: Your input ends with a ${currentArg}, your last argument should be a number`,
-      });
+      throw new SyntaxError(
+        `Error: Your input ends with a ${currentArg}, your last argument should be a number`
+      );
     }
     //Check to make sure that the arguments passed in follow the pattern of number - operator - number etc.
     //Check two operators in a row
     if (operatorRegexTest && nextOperatorRegexTest) {
-      throw new AssertionError({
-        message: `Error: Your input has two or more math operators in a row. The error was found starting with the ${indexOfError} argument: ${currentArg}`,
-      });
+      throw new SyntaxError(
+        `Error: Your input has two or more math operators in a row. The error was found starting with the ${indexOfError} argument: ${currentArg}`
+      );
     }
     //Check two numbers in a row
     if (!numTests && nextNumTests) {
-      throw new AssertionError({
-        message: `Error: Your input has two or more numbers in a row. The error was found starting with the ${indexOfError} argument: ${currentArg}`,
-      });
+      throw new SyntaxError(
+        `Error: Your input has two or more numbers in a row. The error was found starting with the ${indexOfError} argument: ${currentArg}`
+      );
     }
     //Tests all the regexes, if no of pass then there is an invalid input
     if (numTests && !operatorRegexTest) {
-      throw new AssertionError({
-        message: `Error: Your input contains a syntax error. The error was found in the ${indexOfError} argument: ${currentArg}`,
-      });
+      throw new SyntaxError(
+        `Error: Your input contains a syntax error. The error was found in the ${indexOfError} argument: ${currentArg}`
+      );
     }
   }
   return void 0;
